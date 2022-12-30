4a3bfe45630d3165f0400cd19ec63aec87efb5f4
sentey <91223447+sentey@users.noreply.github.com>
2022-12-30 20:28:19 +0000

    cors, req txt

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,5 +6,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path("__reload__/", include("django_browser_reload.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
